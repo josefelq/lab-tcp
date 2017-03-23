7cd19b7f8b1c687bfa5e580eeadc56303ca85043
josefelipeq@live.com <josefelipeq@live.com>
2017-03-22 20:59:11 -0500

    Initial commit

---
+++ Servidor/src/servidor/Conexion.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidor;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ *
+ * @author jose felipe
+ */
+public class Conexion extends Thread{
+    
+    protected Socket socket;
+    
+    protected boolean refuse;
+    
+    public Conexion(Socket clientSocket, boolean refuse) {
+        this.socket = clientSocket;
+        this.refuse=refuse;
+    }
+
+    public void run() {
+        InputStream inp = null;
+        BufferedReader brinp = null;
+        DataOutputStream out = null;
+        try {
+            inp = socket.getInputStream();
+            brinp = new BufferedReader(new InputStreamReader(inp));
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            return;
+        }
+        //chequiar si toca rechazar la conexion
+        if(!refuse){
+        String line;
+        Servidor.incrementarConexiones();
+        while (true) {
+            try {
+                line = brinp.readLine();
+                if ((line == null) || line.equalsIgnoreCase("QUIT")) {
+                    socket.close();
+                    Servidor.reducirConexiones();
+                    return;
+                } else {
+                    out.writeBytes(line + "\n\r");
+                    out.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+      }
+        else{
+        //SEND MESSAGE REFUSING CONNECTION
+        }
+    }
+    
+}
---
+++ Servidor/src/servidor/Servidor.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidor;
+
+import static com.sun.xml.internal.ws.model.RuntimeModeler.PORT;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author jose felipe
+ */
+public class Servidor {
+    
+    //Puerto
+    private static final int PORT = 1978;
+    
+    //Numero maximo de conexiones
+    private static final int MAX_CONEXIONES=10;
+    
+    //Numero actual de conexiones
+    private static int conexiones = 0;
+    
+    //Tamanio buffer
+    private static final int BUFFER=10;
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("I/O error: " + e);
+            }
+            if(conexiones <= MAX_CONEXIONES){
+                new Conexion(socket, false).start();
+            }  
+            else{
+                new Conexion(socket, true).start();
+            }
+        }
+    }
+    
+     public static synchronized void incrementarConexiones() {
+        conexiones++;
+    }
+     
+     public static synchronized void reducirConexiones() {
+        conexiones--;
+    }
+    
+}
